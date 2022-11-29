ef5ec88c722c2f5501e76f026d070ad9dccd2e26
Naveen M K <naveen521kk@gmail.com>
2022-11-29 12:14:00 +0530

    add some error checking

--- test-svg.c
+++ test-svg.c
@@ -7,26 +7,35 @@
 #define HEIGHT 500
 #define TEXT "{ }"
 
-int
-main (void)
+int main(void)
 {
-    cairo_surface_t* surface;
-    cairo_t* cr;
-    PangoFontDescription* font_desc;
-    PangoLayout* layout;
+    cairo_surface_t *surface;
+    cairo_t *cr;
+    PangoFontDescription *font_desc;
+    PangoLayout *layout;
     double width_layout = WIDTH;
-    double font_size_c= 20;
-    PangoAttrList* pango_attr_list = pango_attr_list_new ();
-    const char * filename="test.svg";
+    double font_size_c = 20;
+    PangoAttrList *pango_attr_list = pango_attr_list_new();
+    const char *filename = "test.svg";
 
+    surface = cairo_svg_surface_create(filename, WIDTH, HEIGHT);
+    if (surface == NULL) {
+        fprintf(stderr, "Error: cannot create surface\n");
+        return 1;
+    }
+    cr = cairo_create(surface);
+    if (cr == NULL) {
+        fprintf(stderr, "Error: cannot create cairo context\n");
+        return 1;
+    }
 
-    surface = cairo_svg_surface_create (filename,WIDTH, HEIGHT);
-    cr = cairo_create (surface);
-
-    cairo_move_to(cr,20,20);
+    cairo_move_to(cr, 20, 20);
 
     layout = pango_cairo_create_layout(cr);
-
+    if (layout == NULL){
+        fprintf(stderr, "Error: cannot create pango layout\n");
+        return 1;
+    }
 
     pango_layout_set_width(layout, pango_units_from_double(width_layout));
     font_desc = pango_font_description_new();
@@ -38,15 +47,14 @@ main (void)
     pango_layout_set_font_description(layout, font_desc);
 
     pango_layout_set_text(layout, TEXT, -1);
-    pango_layout_set_wrap(layout,PANGO_WRAP_WORD_CHAR);
+    pango_layout_set_wrap(layout, PANGO_WRAP_WORD_CHAR);
     pango_cairo_show_layout(cr, layout);
 
-
     g_object_unref(layout);
-    pango_attr_list_unref (pango_attr_list);
-    cairo_destroy (cr);
+    pango_attr_list_unref(pango_attr_list);
+    cairo_destroy(cr);
 
-    cairo_surface_destroy (surface);
+    cairo_surface_destroy(surface);
 
     return 0;
 }
